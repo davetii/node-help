c951a17848c6b24af22f391c36bedd140250113d
davetii <davetii@gmail.com>
2024-11-18 20:51:53 -0500

    Add readlines

--- wc/wc.js
+++ wc/wc.js
@@ -1,26 +1,52 @@
-import fs from 'fs/promises';
+import fsp from 'fs/promises';
+import fs from 'fs';
+
+async function readLines(f) {
+  try {
+    const content = await fs.promises.readFile(f, 'utf8');
+    return  content.split('\n').length;
+} catch (err) {
+    console.error(`Error reading file: ${err}`);
+}
+}
 
 async function readBytes(f) {
   try {
-      const stats = await fs.stat(f);
+      const stats = await fsp.stat(f);
       return stats.size;
   } catch (err) {
       console.error(`Error reading file: ${err}`);
   }
 }
 
+async function handleBytes(target) {
+  (async () => {
+    try {
+        const result =  await readBytes(target)
+        console.log(result + " " + target)
+    } catch (error) {
+        console.error(error);
+    }
+  })();
+}
+
+async function handleLines(target) {
+  (async () => {
+    try {
+        const result =  await readLines(target)
+        console.log(result + " " + target)
+    } catch (error) {
+        console.error(error);
+    }
+  })();
+}
+
 if (process.argv.length == 4) {
-  if (process.argv[2] == '-c') {
-    const target = process.argv[3];
-    
-    (async () => {
-      try {
-          const result =  await readBytes(target)
-          console.log(result + " " + target)
-      } catch (error) {
-          console.error(error);
-      }
-    })();
+  const proc = process.argv[2];
+  if ( proc == '-c') {
+    handleBytes(process.argv[3]);
+  } else if (proc == '-l') {
+    handleLines(process.argv[3]);
   } else {
     console.log("Invalid proccess called");
   }
